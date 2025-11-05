721ee626029ffe286e78a2f6c63b2ba7971d10e3
Bernardo Chiamolera <bchiamolera@furb.br>
2025-11-05 20:00:32 -0300

    projeto spring

---
+++ bolsa-valores/src/test/java/org/furb/bolsavalores/BolsaValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.furb.bolsavalores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaValoresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
